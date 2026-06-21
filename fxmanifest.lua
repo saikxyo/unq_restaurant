b81b6876e93f9be1a0e080a576e95fe5f6aa6612
saikxyo <joaov.dejesus777@gmail.com>
2026-06-20 23:03:31 -0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 
 name 'mx_restaurants'
-author 'Mexico Roleplay / ChatGPT'
+author 'Mexico Roleplay'
 description 'Sistema completo de compra, administração e operação de restaurantes para Qbox JS + ox_lib + ox_target + ox_inventory'
 version '1.0.0'
 
